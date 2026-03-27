366d510f31368a0a0ba77449f493f920ee0a19b7
Egor Merkushev <gorkaedeep@gmail.com>
2026-03-27 16:27:16 +0300

    Initialize project with multiplier utility and tests

---
+++ multiplier.py
@@ -0,0 +1,6 @@
+"""Core multiplication helpers for the project."""
+
+
+def multiply(a: int | float, b: int | float) -> int | float:
+    """Return the product of two numeric values."""
+    return a * b
